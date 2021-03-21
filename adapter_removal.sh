2966543e0f774d7804ae8c24a045772f1ad0ebba
Samarth Mathur <mathur20@purdue.edu>
2021-03-21 19:36:11 -0400

    Update adapter removal

--- adapter_removal.sh
+++ adapter_removal.sh
@@ -32,40 +32,49 @@ cd $SLURM_SUBMIT_DIR
 while read -a line
 do 
 	echo "#!/bin/sh -l
-#SBATCH -A PAS1533
+#SBATCH -A gcore
 #SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -t 150:00:00
-#SBATCH --job-name=${line}_adptrem
-#SBATCH -e ${line}_adptrem
-#SBATCH -o ${line}_adptrem
+#SBATCH -n 5
+#SBATCH -t 300:00:00
+#SBATCH --job-name=${line[0]}_adptrim
+#SBATCH -e ${line[0]}_adptrim
+#SBATCH -o ${line[0]}_adptrim
 
 cd $SLURM_SUBMIT_DIR
+module load bioinfo
 module load fastqc/0.11.8
 #module load trimmomatic
 
 # Step1: FASTQC on raw reads
 
-cd /fs/scratch/PAS1533/smathur/wgr/read_preprocess/fastqc/trimmed/
+# unzip reads
+
+#cd /scratch/snyder/m/mathur20/MQU/ch3/reads/r4/
+#unzip *zip
+
+
+# FASTQC on raw reads
+
+cd /scratch/snyder/m/mathur20/MQU/ch3/align/raw
 mkdir ${line}
 
 fastqc -o ${line}/ \
-/fs/scratch/PAS1533/smathur/wgr/read_preprocess/trimmomatic/trimmed/paired/${line}*R1*fastq.paired \
-/fs/scratch/PAS1533/smathur/wgr/read_preprocess/trimmomatic/trimmed/paired/${line}*R2*fastq.paired
+/scratch/snyder/m/mathur20/MQU/ch3/reads/r4/${line[0]}_R1.fastq \
+/scratch/snyder/m/mathur20/MQU/ch3/reads/r4/${line[0]}_R2.fastq \
 
-#cd /fs/scratch/PAS1533/smathur/wgr/read_preprocess/fastqc/raw/${line}
-#unzip *zip
+
 #
 ### Step2: Adapter removal 
 ##
 ##java -jar /apps/trimmomatic/0.38/trimmomatic-0.38.jar \
-##PE /fs/scratch/PAS1533/smathur/wgr/data/raw/${line[0]} /fs/scratch/PAS1533/smathur/wgr/data/raw/${line[1]} \
-###/fs/scratch/PAS1533/smathur/wgr/data/trimmed/${line[0]}.paired /fs/scratch/PAS1533/smathur/wgr/data/trimmed/${line[0]}.unpaired \
-##/fs/scratch/PAS1533/smathur/wgr/data/trimmed/${line[1]}.paired /fs/scratch/PAS1533/smathur/wgr/data/trimmed/${line[1]}.unpaired \
+##PE /scratch/snyder/m/mathur20/MQU/ch3/reads/r4/${line[0]}_R1.fastq /scratch/snyder/m/mathur20/MQU/ch3/reads/r4/${line[0]}_R2.fastq \
+###/scratch/snyder/m/mathur20/MQU/ch3/align/trimmed/${line[0]}.paired /scratch/snyder/m/mathur20/MQU/ch3/align/trimmed/${line[0]}.unpaired \
+##/scratch/snyder/m/mathur20/MQU/ch3/align/trimmed/${line[0]}.paired /scratch/snyder/m/mathur20/MQU/ch3/align/trimmed/${line[0]}.unpaired \
 ##LEADING:20 TRAILING:20 MINLEN:30 \
-##ILLUMINACLIP:/apps/trimmomatic/0.38/adapters/NexteraPE-PE.fa:2:40:10" \
-> /fs/scratch/PAS1533/smathur/wgr/jobcodes/per_ind/${line}/${line}_adptrem.sh
-done < /fs/scratch/PAS1533/smathur/wgr/data/sample.list
+##ILLUMINACLIP:/apps/trimmomatic/0.38/adapters/NexteraPE-PE.fa:2:40:10"
+> /scratch/snyder/m/mathur20/MQU/ch3/jobcodes/mapping/align/${line[0]}_chick_alignment.sh
+done < /scratch/snyder/m/mathur20/MQU/ch3/reads/sample.list.all
+
 #
 #
 #while read -a line
